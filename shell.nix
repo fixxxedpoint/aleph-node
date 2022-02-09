d718ca80adc175577e62463875d65ea09a88dab9
Łukasz Lachowski <lukasz.lachowski@cardinals.cc>
2022-02-09 15:39:43 +0100

    WIP towards working nix-shell

--- shell.nix
+++ shell.nix
@@ -9,30 +9,37 @@ let
     extensions = [ "rust-src" ];
     targets = [ "wasm32-unknown-unknown" ];
   });
-  binutils-unwrapped' = nixpkgs.binutils-unwrapped.overrideAttrs (old: {
-    name = "binutils-2.37";
-    src = nixpkgs.fetchurl {
-      url = "https://ftp.gnu.org/gnu/binutils/binutils-2.37.tar.xz";
-      sha256 = "sha256-gg2XJPAgo+acszeJOgtjwtsWHa3LDgb8Edwp6x6Eoyw=";
-    };
-    patches = [];
-  });
-  env = nixpkgs.llvmPackages_12.stdenv;
-  cc = nixpkgs.wrapCCWith rec {
-    cc = env.cc;
-    bintools = nixpkgs.wrapBintoolsWith {
-      bintools = binutils-unwrapped';
-    };
-  };
-  minimalMkShell = nixpkgs.mkShell.override {
-    # stdenv = nixpkgs.stdenvNoCC;
-    # stdenv = nixpkgs.clangStdenv;
-    stdenv = nixpkgs.overrideCC env cc;
-  };
+  # binutils-unwrapped' = nixpkgs.binutils-unwrapped.overrideAttrs (old: {
+  #   name = "binutils-2.36.1";
+  #   src = nixpkgs.fetchurl {
+  #     url = "https://ftp.gnu.org/gnu/binutils/binutils-2.36.1.tar.xz";
+  #     sha256 = "e81d9edf373f193af428a0f256674aea62a9d74dfe93f65192d4eae030b0f3b0";
+  #   };
+  #   patches = [];
+  # });
+  # env = nixpkgs.llvmPackages_12.stdenv;
+  # cc = nixpkgs.wrapCCWith rec {
+  #   cc = env.cc;
+  #   bintools = nixpkgs.wrapBintoolsWith {
+  #     bintools = binutils-unwrapped';
+  #     libc = env.cc.bintools.libc;
+  #   };
+  # };
+  # minimalMkShell = nixpkgs.mkShell.override {
+  #   # stdenv = nixpkgs.stdenvNoCC;
+  #   # stdenv = nixpkgs.clangStdenv;
+  #   stdenv = nixpkgs.overrideCC env cc;
+  # };
 in
-with nixpkgs; minimalMkShell {
+# with nixpkgs; minimalMkShell {
+with nixpkgs; mkShell {
   buildInputs = [
     clang_12
+    # libcxx
+    # libcxxabi
+    # llvm_12
+    # llvmPackages.libcxxClang
+    # llvmPackages.libcxxStdenv
     openssl.dev
     pkg-config
     rust-nightly
