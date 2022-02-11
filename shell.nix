82d334d5b954fca607c28b152e2ae625bc7051e4
Łukasz Lachowski <lukasz.lachowski@cardinals.cc>
2022-02-11 18:13:34 +0100

    WIP shell.nix uses llvm.13 instead of 12, cleaned. Binary fails.

--- shell.nix
+++ shell.nix
@@ -7,26 +7,25 @@ let
   nixpkgs = import <nixpkgs> { overlays = [ mozillaOverlay ]; };
   rust-nightly = with nixpkgs; ((rustChannelOf { date = "2021-10-24"; channel = "nightly"; }).rust.override {
     extensions = [ "rust-src" ];
-    targets = [ "x86_64-unknown-linux-gnu", "wasm32-unknown-unknown" ];
+    targets = [ "x86_64-unknown-linux-gnu" "wasm32-unknown-unknown" ];
   });
-  # binutils-unwrapped' = nixpkgs.binutils-unwrapped.overrideAttrs (old: {
-  #   name = "binutils-2.36.1";
-  #   src = nixpkgs.fetchurl {
-  #     url = "https://ftp.gnu.org/gnu/binutils/binutils-2.36.1.tar.xz";
-  #     sha256 = "e81d9edf373f193af428a0f256674aea62a9d74dfe93f65192d4eae030b0f3b0";
-  #   };
-  #   patches = [];
-  # });
-  llvm = nixpkgs.llvmPackages_13;
+  binutils-unwrapped' = nixpkgs.binutils-unwrapped.overrideAttrs (old: {
+    name = "binutils-2.36.1";
+    src = nixpkgs.fetchurl {
+      url = "https://ftp.gnu.org/gnu/binutils/binutils-2.36.1.tar.xz";
+      sha256 = "e81d9edf373f193af428a0f256674aea62a9d74dfe93f65192d4eae030b0f3b0";
+    };
+    patches = [];
+  });
+  llvm = nixpkgs.llvmPackages_12;
   env = llvm.stdenv;
-  # cc = nixpkgs.wrapCCWith rec {
-  #   cc = env.cc;
-  #   bintools = nixpkgs.wrapBintoolsWith {
-  #     bintools = binutils-unwrapped';
-  #   };
-  # };
-  # customEnv = nixpkgs.overrideCC env cc;
-  customEnv = env;
+  cc = nixpkgs.wrapCCWith rec {
+    cc = env.cc;
+    bintools = nixpkgs.wrapBintoolsWith {
+      bintools = binutils-unwrapped';
+    };
+  };
+  customEnv = nixpkgs.overrideCC env cc;
 in
 with nixpkgs; customEnv.mkDerivation rec {
   name = "aleph-node";
@@ -34,8 +33,7 @@ with nixpkgs; customEnv.mkDerivation rec {
 
   buildInputs = [
     llvm.clang
-    # binutils-unwrapped'
-    llvm.lld
+    binutils-unwrapped'
     openssl.dev
     pkg-config
     rust-nightly
@@ -44,16 +42,15 @@ with nixpkgs; customEnv.mkDerivation rec {
   ];
 
   shellHook = ''
-    # export CARGO_HOME="$out/cargo"
     export RUST_SRC_PATH="${rust-nightly}/lib/rustlib/src/rust/src"
     export LIBCLANG_PATH="${llvm.libclang.lib}/lib"
     export PROTOC="${protobuf}/bin/protoc"
     export CFLAGS=" \
-        ${"-isystem ${llvm.libclang.lib}/lib/clang/${lib.getVersion llvm.stdenv.cc.cc}/include"} \
+        ${"-isystem ${llvm.libclang.lib}/lib/clang/13.0.0/include"} \
         $CFLAGS
     "
     export CXXFLAGS+=" \
-        ${"-isystem ${llvm.libclang.lib}/lib/clang/${lib.getVersion llvm.stdenv.cc.cc}/include"} \
+        ${"-isystem ${llvm.libclang.lib}/lib/clang/13.0.0/include"} \
         $CXXFLAGS
     "
     # From: https://github.com/NixOS/nixpkgs/blob/1fab95f5190d087e66a3502481e34e15d62090aa/pkgs/applications/networking/browsers/firefox/common.nix#L247-L253
@@ -62,20 +59,18 @@ with nixpkgs; customEnv.mkDerivation rec {
     # uses LLVM's libclang. To make sure all necessary flags are
     # included we need to look in a few places.
     export BINDGEN_EXTRA_CLANG_ARGS=" \
-        ${"-isystem ${llvm.libclang.lib}/lib/clang/${lib.getVersion llvm.stdenv.cc.cc}/include"} \
+        ${"-isystem ${llvm.libclang.lib}/lib/clang/13.0.0/include"} \
         $BINDGEN_EXTRA_CLANG_ARGS
     "
-    # export RUSTFLAGS="-C linker=clang -C link-arg=-fuse-ld=lld -C target-cpu=cascadelake $RUSTFLAGS"
-    # export RUSTFLAGS="-C link-arg=-fuse-ld=lld -C target-cpu=cascadelake $RUSTFLAGS"
-    # export RUSTFLAGS="-C linker=lld -C target-cpu=cascadelake $RUSTFLAGS"
-    # export CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_LINKER="lld"
-    # export RUSTFLAGS="-C target-cpu=cascadelake $RUSTFLAGS"
+    export RUSTFLAGS="-C target-cpu=cascadelake $RUSTFLAGS"
+    export CARGO_BUILD_TARGET="x86_64-unknown-linux-gnu"
   '';
 
   buildPhase = ''
     ${shellHook}
+    export CARGO_HOME="$out/cargo"
 
-    cargo build -vv --release -p aleph-node
+    cargo build --release -p aleph-node
   '';
 
   installPhase = ''
