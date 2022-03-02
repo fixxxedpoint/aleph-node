ff138b25b8cc4ef273f9e8221aa700698cc3e127
Łukasz Lachowski <lukasz.lachowski@cardinals.cc>
2022-03-02 22:53:02 +0100

    default.nix: rocksdb now uses snappy for compatibility with testnet's chainspec

--- default.nix
+++ default.nix
@@ -39,7 +39,7 @@ let
        "-DWITH_TOOLS=0"
        "-DWITH_BZ2=0"
        "-DWITH_LZ4=0"
-       "-DWITH_SNAPPY=0"
+       "-DWITH_SNAPPY=1"
        "-DWITH_ZLIB=0"
        "-DWITH_ZSTD=0"
        "-DWITH_GFLAGS=0"
@@ -50,7 +50,7 @@ let
 
     propagatedBuildInputs = [];
 
-    buildInputs = [];
+    buildInputs = [ nixpkgs.snappy ];
   });
 
   # declares a build environment where C and C++ compilers are delivered by the llvm/clang project
