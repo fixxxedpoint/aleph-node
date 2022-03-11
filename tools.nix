934dcf5c1f0595b96bb6d3fa2b99a5866adfc047
Łukasz Lachowski <lukasz.lachowski@cardinals.cc>
2022-03-11 13:22:34 +0100

    WIP debugging tools.nix

--- tools.nix
+++ tools.nix
@@ -42,6 +42,7 @@ rec {
 
         mkdir -p "$out/cargo"
 
+        export CARGO="${pkgs.cargo}/bin/cargo"
         export CARGO_HOME="$out/cargo"
         export HOME="$out"
         export CARGO_NET_GIT_FETCH_WITH_CLI=true
@@ -52,12 +53,31 @@ rec {
 
         set -x
 
-        {
-          crate2nix generate \
-            $create2nix_options \
-            -o "Cargo-generated.nix" \
-            -h "$crate_hashes" \
-            ${lib.escapeShellArgs additionalCargoNixArgs}
+        crate2nix generate \
+          $create2nix_options \
+          -o "Cargo-generated.nix" \
+          -h "$crate_hashes" \
+          ${lib.escapeShellArgs additionalCargoNixArgs} || {
+          { set +x; } 2>/dev/null
+          echo "crate2nix failed." >&2
+          echo "== cargo/config (BEGIN)" >&2
+          sed 's/^/    /' $out/cargo/config >&2
+          echo ""
+          echo "== cargo/config (END)" >&2
+            echo ""
+            echo "== crate-hashes.json (BEGIN)" >&2
+          if [ -r $crate_hashes ]; then
+            sed 's/^/    /' $crate_hashes >&2
+            echo ""
+          else
+            echo "$crate_hashes missing"
+          fi
+          echo "== crate-hashes.json (END)" >&2
+          echo ""
+          echo "== ls -la (BEGIN)" >&2
+          ls -la
+          echo "== ls -la (END)" >&2
+          exit 3
         }
         { set +x; } 2>/dev/null
 
