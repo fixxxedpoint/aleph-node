f02c29fb7fe67c90edce28a9c14b7ba2d75075b5
Łukasz Lachowski <lukasz.lachowski@cardinals.cc>
2022-03-11 15:21:49 +0100

    WIP cleaned tools.nix, moved to ./nix/

--- default.nix
+++ default.nix
@@ -107,7 +107,7 @@ let
     url = "https://github.com/NixOS/nixpkgs/archive/c82b46413401efa740a0b994f52e9903a4f6dcd5.tar.gz";
     sha256 = "13s8g6p0gzpa1q6mwc2fj2v451dsars67m4mwciimgfwhdlxx0bk";
   }){}).crate2nix;
-  crate2nixTools = import ./tools.nix { pkgs = nixpkgs; lib = nixpkgs.lib; stdenv = env; inherit crate2nix; };
+  crate2nixTools = import ./nix/tools.nix { pkgs = nixpkgs; lib = nixpkgs.lib; stdenv = env; inherit crate2nix; };
   generatedCargoNix = crate2nixTools.generatedCargoNix {
     name = "aleph-node";
     src = ./.;
