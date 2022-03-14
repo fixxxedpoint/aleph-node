5103f70cafd246af6199acf3fb99bb09c32cea61
Łukasz Lachowski <lukasz.lachowski@cardinals.cc>
2022-03-14 21:33:13 +0100

    WIP fixing cargo-chef

--- nix/cargo-chef.nix
+++ nix/cargo-chef.nix
@@ -4,30 +4,6 @@
 , crateDir
 }:
 let
-  # cargo-chef =
-  #   let
-  #     cargo-chef = buildRustCrate {
-  #       crateName = "cargo-chef";
-  #       version = "0.1.34";
-  #       sha256 = "XVCCAQeh9bz3Pp0pLgKXizvF8EtagksHjwNz0U//xQs=";
-  #     };
-  #   in
-  #   builtins.symlinkJoin { name = "cargo-chef"; paths = [ cargo-chef ]; };
-
-  # cargo-chef = buildRustCrate {
-  #   crateName = "cargo-chef";
-  #   version = "0.1.34";
-  #   sha256 = "XVCCAQeh9bz3Pp0pLgKXizvF8EtagksHjwNz0U//xQs=";
-  # };
-
-  # cargo-chef = pkgs.runCommand "cargo-chef" { nativeBuildInputs = [ pkgs.cargo pkgs.rustc pkgs.cacert ]; } ''
-  #   export CARGO_HOME=$out/cargo
-  #   mkdir -p $CARGO_HOME
-  #   mkdir -p $out/bin
-  #   cargo install cargo-chef --locked
-  #   cp $CARGO_HOME/bin/cargo-chef $out/bin/
-  # '';
-
   cargo-chef = (pkgs.runCommandCC "cargo-chef" { nativeBuildInputs = [ pkgs.cargo pkgs.rustc pkgs.cacert ]; } ''
     export CARGO_HOME=$out/cargo
     mkdir -p $CARGO_HOME
@@ -41,16 +17,16 @@ let
     cargo-chef chef prepare --recipe-path $out
   '';
 
-  cachedDependencies = recipeJson: pkgs.runCommand "cargo-chef cook" { nativeBuildInputs = [ cargo-chef pkgs.cargo pkgs.rustc pkgs.cacert ]; } ''
+  storedRecipe = builtins.toFile "recipe.json" (builtins.readFile buildRecipe);
+
+  cachedDependencies = recipeJson: (pkgs.runCommandCC "cargo-chef cook" { nativeBuildInputs = [ cargo-chef pkgs.cargo pkgs.rustc pkgs.cacert ]; } ''
     TMP=$out/tmp
     export CARGO_HOME=$TMP/.cargo-home
     mkdir -p $CARGO_HOME
-    echo ${recipeJson} >$TMP/recipe.json
 
-    yes yes | cargo-chef chef cook --recipe-path $TMP/recipe.json
+    yes yes | cargo-chef chef cook --recipe-path ${recipeJson} || true
     mv $CARGO_HOME/* $out/
     rm -rf $TMP
-  '';
-
+  '').overrideAttrs (_: { inherit stdenv; });
 in
-cachedDependencies (builtins.readFile "${buildRecipe}")
+cachedDependencies storedRecipe
