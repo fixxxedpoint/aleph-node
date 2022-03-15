95ff0b149136579dcb6568a0ab13b7caea943f76
Łukasz Lachowski <lukasz.lachowski@cardinals.cc>
2022-03-15 01:01:38 +0100

    WIP fixing docker.nix

--- docker/docker.nix
+++ docker/docker.nix
@@ -1,8 +1,9 @@
 { pkgs ? import ../nix/nixpkgs.nix {}
 }:
 let
+  # use newest nixpkgs instead of that version
   alephNode = (import ../nix/aleph-node.nix {}).workspaceMembers."aleph-node".build;
-  buildDependencies = pkgs.lib.unique (alephNode.completeDeps ++ alephNode.completeBuildDeps);
+  buildDependencies = pkgs.lib.unique (alephNode.completeDeps ++ alephNode.completeBuildDeps ++ alephNode.nativeBuildInputs ++ alephNode.buildInputs);
 in
 pkgs.dockerTools.streamLayeredImage {
   name = "aleph_build_image";
