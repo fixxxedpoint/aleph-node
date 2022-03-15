3baa4d62f9692a51900596b752255f6bcbeeb3d5
Łukasz Lachowski <lukasz.lachowski@cardinals.cc>
2022-03-15 12:35:32 +0100

    WIP using nixpkgs-unstable for docker

--- docker/docker.nix
+++ docker/docker.nix
@@ -1,7 +1,7 @@
 let
   nixpkgs = import (builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/a7ecde854aee5c4c7cd6177f54a99d2c1ff28a31.tar.gz";
-    sha256 = "162dywda2dvfj1248afxc45kcrg83appjd0nmdb541hl7rnncf02";
+    url = "https://github.com/NixOS/nixpkgs/archive/be872a7453a176df625c12190b8a6c10f6b21647.tar.gz";
+    sha256 = "762dywda2dvfj1248afxc45kcrg83appjd0nmdb541hl7rnncf02";
   }) {};
 
   # use newest nixpkgs instead of that version
