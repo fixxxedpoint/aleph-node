2d03abd71672eb52f4ecf350b66d5df7d5eeb185
Łukasz Lachowski <lukasz.lachowski@cardinals.cc>
2022-03-16 00:43:01 +0100

    WIP fixing importCargoLock.nix

--- nix/importCargoLock.nix
+++ nix/importCargoLock.nix
@@ -1,7 +1,4 @@
-let
-  importCargoLock = import (builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/be872a7453a176df625c12190b8a6c10f6b21647.tar.gz";
-    sha256 = "1hnwh2w5rhxgbp6c8illcrzh85ky81pyqx9309bkgpivyzjf2nba";
-  } {}).rustPlatform.importCargoLock;
-in
-importCargoLock
+(import (builtins.fetchTarball {
+  url = "https://github.com/NixOS/nixpkgs/archive/be872a7453a176df625c12190b8a6c10f6b21647.tar.gz";
+  sha256 = "1hnwh2w5rhxgbp6c8illcrzh85ky81pyqx9309bkgpivyzjf2nba";
+}) {}).rustPlatform.importCargoLock
