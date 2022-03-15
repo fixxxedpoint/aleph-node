a03c8c778e7fbfd40709e9e2e66dacc74923e9be
Łukasz Lachowski <lukasz.lachowski@cardinals.cc>
2022-03-15 12:30:24 +0100

    WIP updated sha256 for nix packages in docker.nix

--- docker/docker.nix
+++ docker/docker.nix
@@ -1,7 +1,7 @@
 let
   nixpkgs = import (builtins.fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/a7ecde854aee5c4c7cd6177f54a99d2c1ff28a31.tar.gz";
-    sha256 = "0admybxrjan9a04wq54c3zykpw81sc1z1nqclm74a7pgjdp7iqv1";
+    sha256 = "162dywda2dvfj1248afxc45kcrg83appjd0nmdb541hl7rnncf02";
   }) {};
 
   # use newest nixpkgs instead of that version
