8c66f5100469e95c6551bdaf7fc4f3c24ce7bcdf
Łukasz Lachowski <lukasz.lachowski@cardinals.cc>
2022-03-15 14:20:11 +0100

    WIP using buildImage in docker.nix

--- docker/docker.nix
+++ docker/docker.nix
@@ -1,14 +1,13 @@
 let
   nixpkgs = import (builtins.fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/be872a7453a176df625c12190b8a6c10f6b21647.tar.gz";
-    sha256 = "762dywda2dvfj1248afxc45kcrg83appjd0nmdb541hl7rnncf02";
+    sha256 = "1hnwh2w5rhxgbp6c8illcrzh85ky81pyqx9309bkgpivyzjf2nba";
   }) {};
 
-  # use newest nixpkgs instead of that version
   alephNode = (import ../nix/aleph-node.nix {}).workspaceMembers."aleph-node".build;
   buildDependencies = nixpkgs.lib.unique (alephNode.completeDeps ++ alephNode.completeBuildDeps ++ alephNode.nativeBuildInputs ++ alephNode.buildInputs);
 in
-nixpkgs.dockerTools.streamLayeredImage {
+nixpkgs.dockerTools.buildImage {
   name = "aleph_build_image";
-  contents = [ buildDependencies ];
+  contents = [ buildDependencies nixpkgs.bash ];
 }
