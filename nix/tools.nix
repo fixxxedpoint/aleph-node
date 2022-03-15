b7592f588a261fc1a78dfd4807e54b36bd121e2d
Łukasz Lachowski <lukasz.lachowski@cardinals.cc>
2022-03-15 22:17:04 +0100

    WIP importCargoLock was missing inside of the used nixpkgs - using unstable

--- nix/tools.nix
+++ nix/tools.nix
@@ -23,7 +23,10 @@ rec {
     }:
     let
       cargoLock = builtins.readFile (src + "/Cargo.lock");
-      vendoredCargoLock = pkgs.rustPlatform.importCargoLock cargoLock;
+      vendoredCargoLock = (import (builtins.fetchTarball {
+        url = "https://github.com/NixOS/nixpkgs/archive/be872a7453a176df625c12190b8a6c10f6b21647.tar.gz";
+        sha256 = "1hnwh2w5rhxgbp6c8illcrzh85ky81pyqx9309bkgpivyzjf2nba";
+      }) {}).rustPlatform.importCargoLock cargoLock;
       vendoredCargoConfig = vendoredCargoLock + ".cargo/config";
     in
     stdenv.mkDerivation {
