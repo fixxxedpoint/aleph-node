1a0c6ae34467907234bb88f23c891e789af7d694
Łukasz Lachowski <lukasz.lachowski@cardinals.cc>
2022-03-15 22:50:23 +0100

    WIP syntax error in tools.nix

--- nix/tools.nix
+++ nix/tools.nix
@@ -5,115 +5,117 @@
 , crate2nix ? pkgs.crate2nix
 }:
 let
-  outputHashes = crateDir: rec {
-    toPackageId = { name, version, source, ... }:
-            "${name}-${version}";
+  outputHashes = crateDir:
+    let
+      toPackageId = { name, version, source, ... }:
+              "${name}-${version}";
 
-    lockFiles =
-      let
-        fromCrateDir =
-          if builtins.pathExists (crateDir + "/Cargo.lock")
-          then [ (crateDir + "/Cargo.lock") ]
-          else [ ];
-        fromSources =
-          if builtins.pathExists (crateDir + "/crate2nix-sources")
-          then
-            let
-              subdirsTypes = builtins.readDir (crateDir + "/crate2nix-sources");
-              subdirs = builtins.attrNames subdirsTypes;
-              toLockFile = subdir: (crateDir + "/crate2nix-sources/${subdir}/Cargo.lock");
-            in
-            builtins.map toLockFile subdirs
-          else [ ];
-      in
-      fromCrateDir ++ fromSources;
+      lockFiles =
+        let
+          fromCrateDir =
+            if builtins.pathExists (crateDir + "/Cargo.lock")
+            then [ (crateDir + "/Cargo.lock") ]
+            else [ ];
+          fromSources =
+            if builtins.pathExists (crateDir + "/crate2nix-sources")
+            then
+              let
+                subdirsTypes = builtins.readDir (crateDir + "/crate2nix-sources");
+                subdirs = builtins.attrNames subdirsTypes;
+                toLockFile = subdir: (crateDir + "/crate2nix-sources/${subdir}/Cargo.lock");
+              in
+              builtins.map toLockFile subdirs
+            else [ ];
+        in
+        fromCrateDir ++ fromSources;
 
-    locked =
-      let
-        parseFile = cargoLock: builtins.fromTOML (builtins.readFile cargoLock);
-        allParsedFiles = builtins.map parseFile lockFiles;
-        merge = merged: lock:
-          {
-            package = merged.package ++ lock.package or [ ];
-            metadata = merged.metadata // lock.metadata or { };
-          };
-      in
-      lib.foldl merge { package = [ ]; metadata = { }; } allParsedFiles;
+      locked =
+        let
+          parseFile = cargoLock: builtins.fromTOML (builtins.readFile cargoLock);
+          allParsedFiles = builtins.map parseFile lockFiles;
+          merge = merged: lock:
+            {
+              package = merged.package ++ lock.package or [ ];
+              metadata = merged.metadata // lock.metadata or { };
+            };
+        in
+        lib.foldl merge { package = [ ]; metadata = { }; } allParsedFiles;
 
-    hashesFiles =
-      builtins.map
-        (cargoLock: "${dirOf cargoLock}/crate-hashes.json")
-        lockFiles;
+      hashesFiles =
+        builtins.map
+          (cargoLock: "${dirOf cargoLock}/crate-hashes.json")
+          lockFiles;
 
-    hashes =
-      let
-        parseFile = hashesFile:
-          if builtins.pathExists hashesFile
-          then builtins.fromJSON (builtins.readFile hashesFile)
-          else { };
-        parsedFiles = builtins.map parseFile hashesFiles;
-      in
-      lib.foldl (a: b: a // b) { } parsedFiles;
+      hashes =
+        let
+          parseFile = hashesFile:
+            if builtins.pathExists hashesFile
+            then builtins.fromJSON (builtins.readFile hashesFile)
+            else { };
+          parsedFiles = builtins.map parseFile hashesFiles;
+        in
+        lib.foldl (a: b: a // b) { } parsedFiles;
 
-    unhashedGitDeps = builtins.filter (p: ! hashes ? ${toPackageId p}) packagesByType.git or [ ];
+      unhashedGitDeps = builtins.filter (p: ! hashes ? ${toPackageId p}) packagesByType.git or [ ];
 
-    # Extracts URL and rev from a git source URL.
-    #
-    # Crude, should be more robust :(
-    parseGitSource = source:
-      assert builtins.isString source;
-      let
-        withoutGitPlus = lib.removePrefix "git+" source;
-        splitHash = lib.splitString "#" withoutGitPlus;
-        splitQuestion = lib.concatMap (lib.splitString "?") splitHash;
-      in
-      {
-        url = builtins.head splitQuestion;
-        rev = lib.last splitQuestion;
-      };
-
-    mkGitHash = { source, ... }@attrs:
-      let
-        parsed = parseGitSource source;
-        src = builtins.fetchGit {
-          submodules = true;
-          inherit (parsed) url rev;
-          allRefs = true;
+      # Extracts URL and rev from a git source URL.
+      #
+      # Crude, should be more robust :(
+      parseGitSource = source:
+        assert builtins.isString source;
+        let
+          withoutGitPlus = lib.removePrefix "git+" source;
+          splitHash = lib.splitString "#" withoutGitPlus;
+          splitQuestion = lib.concatMap (lib.splitString "?") splitHash;
+        in
+        {
+          url = builtins.head splitQuestion;
+          rev = lib.last splitQuestion;
         };
-        hash = pkgs.runCommand "hash-of-${attrs.name}" { nativeBuildInputs = [ pkgs.nix ]; } ''
-          echo -n "$(nix-hash --type sha256 ${src})" > $out
-        '';
-      in
-      {
-        name = toPackageId attrs;
-        value = builtins.readFile hash;
-      };
 
-    extraHashes = builtins.listToAttrs (map mkGitHash unhashedGitDeps);
+      mkGitHash = { source, ... }@attrs:
+        let
+          parsed = parseGitSource source;
+          src = builtins.fetchGit {
+            submodules = true;
+            inherit (parsed) url rev;
+            allRefs = true;
+          };
+          hash = pkgs.runCommand "hash-of-${attrs.name}" { nativeBuildInputs = [ pkgs.nix ]; } ''
+            echo -n "$(nix-hash --type sha256 ${src})" > $out
+          '';
+        in
+        {
+          name = toPackageId attrs;
+          value = builtins.readFile hash;
+        };
 
-    sourceType = { source ? null, ... } @ package:
-      assert source == null || builtins.isString source;
+      extraHashes = builtins.listToAttrs (map mkGitHash unhashedGitDeps);
 
-      if source == null then
-        null
-      else if source == "registry+https://github.com/rust-lang/crates.io-index" then
-        "crates-io"
-      else if lib.hasPrefix "git+" source then
-        "git"
-      else
-        builtins.throw "unknown source type: ${source}";
+      sourceType = { source ? null, ... } @ package:
+        assert source == null || builtins.isString source;
 
-    packages =
-      let
-        packagesWithDuplicates = assert builtins.isList locked.package; locked.package;
-        packagesWithoutLocal = builtins.filter (p: p ? source) packagesWithDuplicates;
-        packageById = package: { name = toPackageId package; value = package; };
-        packagesById = builtins.listToAttrs (builtins.map packageById packagesWithoutLocal);
-      in
-      builtins.attrValues packagesById;
-    packagesWithType = builtins.filter (pkg: (sourceType pkg) != null) packages;
-    packagesByType = lib.groupBy sourceType packagesWithType;
-  };
+        if source == null then
+          null
+        else if source == "registry+https://github.com/rust-lang/crates.io-index" then
+          "crates-io"
+        else if lib.hasPrefix "git+" source then
+          "git"
+        else
+          builtins.throw "unknown source type: ${source}";
+
+      packages =
+        let
+          packagesWithDuplicates = assert builtins.isList locked.package; locked.package;
+          packagesWithoutLocal = builtins.filter (p: p ? source) packagesWithDuplicates;
+          packageById = package: { name = toPackageId package; value = package; };
+          packagesById = builtins.listToAttrs (builtins.map packageById packagesWithoutLocal);
+        in
+        builtins.attrValues packagesById;
+      packagesWithType = builtins.filter (pkg: (sourceType pkg) != null) packages;
+      packagesByType = lib.groupBy sourceType packagesWithType;
+  in
+  extraHashes;
 in
 rec {
 
