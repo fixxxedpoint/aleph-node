be8cf30ff0fa18e366fc5a3c8311c5c69ce4ea8b
Łukasz Lachowski <lukasz.lachowski@cardinals.cc>
2022-03-15 22:19:49 +0100

    WIP fixing importCargoLock

--- nix/tools.nix
+++ nix/tools.nix
@@ -23,10 +23,12 @@ rec {
     }:
     let
       cargoLock = builtins.readFile (src + "/Cargo.lock");
-      vendoredCargoLock = (import (builtins.fetchTarball {
+      importCargoLock = (import (builtins.fetchTarball {
         url = "https://github.com/NixOS/nixpkgs/archive/be872a7453a176df625c12190b8a6c10f6b21647.tar.gz";
         sha256 = "1hnwh2w5rhxgbp6c8illcrzh85ky81pyqx9309bkgpivyzjf2nba";
-      }) {}).rustPlatform.importCargoLock cargoLock;
+      }) {}).rustPlatform.importCargoLock;
+
+      vendoredCargoLock = importCargoLock cargoLock;
       vendoredCargoConfig = vendoredCargoLock + ".cargo/config";
     in
     stdenv.mkDerivation {
