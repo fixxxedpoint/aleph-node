4708d781fbebb16a97c3e84130b56572ca13f750
Łukasz Lachowski <lukasz.lachowski@cardinals.cc>
2022-03-17 11:46:02 +0100

    WIP defining docker containers - build container and run container

--- docker/docker.nix
+++ docker/docker.nix
@@ -1,11 +1,8 @@
 let
   versions = import ../nix/versions.nix;
   nixpkgs = versions.dockerNixpkgs;
-
-  fetchDependenciesAttrs = nixpkgs.lib.filterAttrs (n: _: nixpkgs.lib.hasPrefix "fetch" n) versions;
-  fetchDependencies = nixpkgs.lib.mapAttrsToList (_: v: v) fetchDependenciesAttrs;
-
   nixFromDockerHub = nixpkgs.dockerTools.pullImage {
+
     imageName = "nixos/nix";
     imageDigest = "sha256:f0c68f870c655d8d96658ca762a0704a30704de22d16b4956e762a2ddfbccb09";
     sha256 = "sha256-pje9GziBsB28BDzhNSnrphN6xzxxsEnP2YDp7zAED8o=";
@@ -15,22 +12,27 @@ let
 
   alephNodeDrv = import ../nix/aleph-node.nix {};
   alephNode = alephNodeDrv.project.workspaceMembers."aleph-node".build;
-  toolsDependencies = [ alephNodeDrv.generated.buildInputs alephNodeDrv.generated.nativeBuildInputs];
-  allBuildDeps = deps: deps ++ (builtins.concatMap (dep: dep.buildInputs ++ dep.nativeBuildInputs) deps);
-  buildDependencies = nixpkgs.lib.unique (
-    (allBuildDeps alephNode.completeDeps) ++
-    (allBuildDeps alephNode.completeBuildDeps) ++
-    alephNode.nativeBuildInputs ++
-    alephNode.buildInputs ++
-    [alephNode.stdenv.cc] ++
-    fetchDependencies ++
-    toolsDependencies ++
-    [nixpkgs.bash]);
+  alephNodeSrc = alephNode.src;
+
+  alephBuildImage = nixpkgs.dockerTools.buildImage {
+    name = "aleph_build_image";
+    contents = [alephNodeSrc];
+    runAsRoot = ''
+      nix-build "${alephNodeSrc}"
+    '';
+    fromImage = nixFromDockerHub;
+    fromImageName = "nixos/nix";
+    fromImageTag = "2.6.0";
+  };
+
+  alephNodeImage = nixpkgs.dockerTools.buildImage {
+    name = "aleph-node";
+    contents = [alephNode "${alephNodeSrc}/docker/docker_entrypoint.sh"];
+    runAsRoot = ''mkdir -p /node'';
+    config = {
+      WorkingDir = "/node";
+      Entrypoint = "./docker_entrypoint.sh";
+    };
+  };
 in
-nixpkgs.dockerTools.buildImage {
-  name = "aleph_build_image";
-  contents = [buildDependencies];
-  fromImage = nixFromDockerHub;
-  fromImageName = "nixos/nix";
-  fromImageTag = "2.6.0";
-}
+alephNodeImage
