00a10d4c67f8d2ea67025c5a404f8748e45048ad
Łukasz Lachowski <lukasz.lachowski@cardinals.cc>
2022-03-17 15:53:21 +0100

    WIP fixed issue wit alephNodeSrc

--- docker/docker.nix
+++ docker/docker.nix
@@ -5,7 +5,7 @@ let
 
   alephNodeDrv = import ../nix/aleph-node.nix {};
   alephNode = alephNodeDrv.project.workspaceMembers."aleph-node".build;
-  alephNodeSrc = alephNodeDrv.project.src;
+  alephNodeSrc = nixpkgs.copyPathToStore ../.;
   dockerEntrypointScript = nixpkgs.writeScriptBin "docker-entrypoint.sh" (builtins.readFile ./docker_entrypoint.sh);
 
   alephNodeImage = nixpkgs.dockerTools.buildImage {
