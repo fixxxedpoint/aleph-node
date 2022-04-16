a1487ebbef656a5f1159c5e13eda6c9e09ab127d
Łukasz Lachowski <lukasz.lachowski@cardinals.cc>
2022-04-16 12:00:45 +0200

    WIP fixing import-cargo fetchTarball

--- nix/tools.nix
+++ nix/tools.nix
@@ -79,10 +79,10 @@ rec {
 
   importCargo =
     let
-      importCargoSrc = import (builtins.fetchTarball {
+      importCargoSrc = builtins.fetchTarball {
         url = "https://github.com/edolstra/import-cargo/archive/25d40be4a73d40a2572e0cc233b83253554f06c5.tar.gz";
         sha256 = "0dnwaz58s7pcfdvwi0crmx8cqlxi7il623n126db9nkq0fp41fvy";
-      });
+      };
       importCargo = (import flakeCompat { src = importCargoSrc; }).defaultNix;
     in
     importCargo;
