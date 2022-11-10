3d83faef5944878258e817052da088c95a7e982f
Łukasz Lachowski <lukasz.lachowski@cardinals.cc>
2022-11-10 21:37:05 +0100

    WIP mutex based import_queue

--- bin/node/src/import_queue.rs
+++ bin/node/src/import_queue.rs
@@ -1,21 +1,23 @@
+use std::sync::{Arc, Mutex};
+
 use futures::channel::mpsc::{unbounded, UnboundedSender};
 use sc_consensus::ImportQueue;
 use sp_runtime::traits::Block;
 
-pub struct SplitImportQueue<B: Block> {
-    import_block_sender: UnboundedSender<(
-        sp_consensus::BlockOrigin,
-        Vec<sc_consensus::IncomingBlock<B>>,
-    )>,
-    import_finalization_sender: UnboundedSender<(
-        sp_consensus::BlockOrigin,
-        Vec<sc_consensus::IncomingBlock<B>>,
-    )>,
+#[derive(Clone)]
+pub struct SplitImportQueue<B: Block, IQ: ImportQueue<B>> {
+    import_queue: Arc<Mutex<IQ>>,
 }
 
-pub struct SplittedImportQueue {}
-
 // TODO zamiast tego, mozna zrobic cos podobnego do splita, czyli jeden ImportQueue, pod mutexem, i oba o niego konkuruja
+
+impl<B: Block, IQ: ImportQueue<B>> SplitImportQueue<B, IQ> {
+    fn get_import_queue(&mut self) -> impl DerefMut<IQ> {
+        self.import_queue
+            .lock()
+            .unwrap_or_else(|_| panic!("unable to use `import_queue`"))
+    }
+}
 
 impl<B: Block> ImportQueue<B> for SplitImportQueue {
     fn import_blocks(
@@ -23,7 +25,7 @@ impl<B: Block> ImportQueue<B> for SplitImportQueue {
         origin: sp_consensus::BlockOrigin,
         blocks: Vec<sc_consensus::IncomingBlock<B>>,
     ) {
-        todo!()
+        (&mut *self.get_import_queue()).import_blocks(origin, blocks);
     }
 
     fn import_justifications(
@@ -33,7 +35,7 @@ impl<B: Block> ImportQueue<B> for SplitImportQueue {
         number: sp_api::NumberFor<B>,
         justifications: sp_runtime::Justifications,
     ) {
-        todo!()
+        (&mut *self.get_import_queue()).import_justifications(who, hash, number, justifications);
     }
 
     fn poll_actions(
@@ -41,11 +43,12 @@ impl<B: Block> ImportQueue<B> for SplitImportQueue {
         cx: &mut futures::task::Context,
         link: &mut dyn sc_consensus::Link<B>,
     ) {
-        todo!()
+        (&mut *self.get_import_queue()).poll_actions(cx, link);
     }
 }
 
 fn split<B: Block, IQ: ImportQueue<B>>(iq: IQ) -> (SplitImportQueue, SplitImportQueue) {
-    let (reciever, sender_one) = unbounded();
-    let sender_two = sender_one.clone();
+    let wrapped_import_queue = Arc::new(Mutex::new(iq));
+    let split_import_queue = SplitImportQueue::new(wrapped_import_queue);
+    (split_import_queue.clone(), split_import_queue)
 }
