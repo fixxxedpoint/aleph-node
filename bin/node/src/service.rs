0e1ed2a4d0e6cb9da39e5fe4bff51288d5097ea4
Łukasz Lachowski <lukasz.lachowski@cardinals.cc>
2023-04-17 16:15:08 +0200

    WIP version aligned with new Transport ready version of substrate

--- bin/node/src/service.rs
+++ bin/node/src/service.rs
@@ -243,7 +243,7 @@ fn setup(
         ));
 
     let (network, system_rpc_tx, tx_handler_controller, network_starter) =
-        sc_service::build_network(sc_service::BuildNetworkParams {
+        sc_service::build_network(sc_service::BuildNetworkParams::<_, _, _, _> {
             config: &config,
             client: client.clone(),
             transaction_pool: transaction_pool.clone(),
@@ -251,6 +251,7 @@ fn setup(
             import_queue,
             block_announce_validator_builder: None,
             warp_sync: None,
+            transport: None,
         })?;
 
     let rpc_builder = {
